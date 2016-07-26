0518e820e9b711e700a7bf32ede426c4fe2e7e7c
Jeremy Felt <jeremy.felt@gmail.com>
2016-07-26 15:31:50 -0700

    Update how scripts and styles are enqueued
    Also adjusts the JS to fix syntax errors

--- header.php
+++ header.php
@@ -10,18 +10,6 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-	<!-- STYLES -->
-	<link href="<?php echo get_template_directory_uri(); ?>/repo/styles/skeleton.css" rel="stylesheet" type="text/css">
-	<link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" type="text/css">
-	<link href="<?php echo get_template_directory_uri(); ?>/repo/pika/webfonts/ss-pika.css" rel="stylesheet" />
-
-	<!-- SCRIPTS -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js" type="text/javascript"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js" type="text/javascript"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/repo/js/video/video.js"></script>
-	<link href="<?php echo get_template_directory_uri(); ?>/repo/js/video/video-js.css" rel="stylesheet">
-	<script src="<?php echo get_template_directory_uri(); ?>/repo/js/scripts.js"></script>
-
 	<!-- OUTPUT -->
 	<?php wp_head(); ?>
 
