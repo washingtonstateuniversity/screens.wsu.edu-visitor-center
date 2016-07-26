66c5066cf904fdb1b55159bb855e0e3088d481eb
Jeremy Felt <jeremy.felt@gmail.com>
2016-07-26 15:10:31 -0700

    Escape the output of video URLs

--- content-video.php
+++ content-video.php
@@ -11,7 +11,7 @@ if ( has_tag( 'home' ) ) { $loop = ' loop'; $ytloop = '&loop=1'; } else { $loop 
 <?php if ( '' !== $videolink ) { ?>
 
 	<video class="video-js vjs-default-skin" controls autoplay<?php echo $loop; ?> preload="auto" width="1920" height="1080" poster="" data-setup='{"example_option":true}'>
-		<source src="<?php echo get_post_meta( $post->ID, 'video', true ); ?>" type='video/mp4' />
+		<source src="<?php echo esc_url( get_post_meta( $post->ID, 'video', true ) ); ?>" type='video/mp4' />
 	</video>
 
 	<script>
@@ -22,7 +22,7 @@ if ( has_tag( 'home' ) ) { $loop = ' loop'; $ytloop = '&loop=1'; } else { $loop 
 
 <?php } elseif ( '' !== $youtube ) { ?>
 
-<iframe width="1920" height="1080" src="//www.youtube.com/embed/<?php echo get_post_meta( $post->ID, 'youtube', true ); ?>?version=3&enablejsapi=1&rel=0&showinfo=0&autoplay=1&controls=0&html5=1<?php echo $ytloop; ?>&volume=100" frameborder="0"  scrolling="yes" allowfullscreen></iframe>
+<iframe width="1920" height="1080" src="//www.youtube.com/embed/<?php echo esc_attr( get_post_meta( $post->ID, 'youtube', true ) ); ?>?version=3&enablejsapi=1&rel=0&showinfo=0&autoplay=1&controls=0&html5=1<?php echo $ytloop; ?>&volume=100" frameborder="0"  scrolling="yes" allowfullscreen></iframe>
 
 <?php } ?>
 
